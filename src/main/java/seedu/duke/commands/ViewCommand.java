69e56d241b281301c5f77927f8ebe46cb1bcf910
Lai Ping <>
2020-10-14 23:27:56 +0800

    Add View Function

--- src/main/java/seedu/duke/commands/ViewCommand.java
+++ src/main/java/seedu/duke/commands/ViewCommand.java
@@ -1,4 +1,20 @@
 package seedu.duke.commands;
 
-public class ViewCommand {
+import seedu.duke.data.Transaction;
+import java.util.List;
+
+public class ViewCommand extends Command {
+
+    public static final String COMMAND_WORD = "view";
+    public static final String MESSAGE_SUCCESS = "Above are all transaction entered.";
+
+    @Override
+    public CommandResult execute() {
+
+        List<Transaction> allTransactions = transactionList.getTransactionList();
+        for (int i = 0; i< allTransactions.size(); i++) {
+            System.out.println(allTransactions.indexOf(i));
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
+    }
 }
