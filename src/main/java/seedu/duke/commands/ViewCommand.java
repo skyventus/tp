37b4e95bd28b17e226087b6961e2c8c3ce2434e3
Lai Ping <>
2020-10-15 22:33:01 +0800

    Revert "Add View Function Fix Print out error"
    This reverts commit d40e31bc

--- src/main/java/seedu/duke/commands/ViewCommand.java
+++ src/main/java/seedu/duke/commands/ViewCommand.java
@@ -9,16 +9,12 @@ public class ViewCommand extends Command {
     public static final String COMMAND_WORD = "view";
     public static final String MESSAGE_SUCCESS = "Above are all transaction entered.";
 
-
     @Override
     public CommandResult execute() {
 
         List<Transaction> allTransactions = transactionList.getTransactionList();
-        int count = 1;
-        for (Transaction transaction : allTransactions) {
-            System.out.println(
-                    count  + "." + transaction.getDate() + " " + transaction.getDescription() + " " + transaction.getAmount());
-                count ++;
+        for (int i = 0; i < allTransactions.size(); i++) {
+            System.out.println(allTransactions.indexOf(i));
         }
         return new CommandResult(String.format(MESSAGE_SUCCESS));
     }
