c17b2fdb20a8e9cc752327691914474c1dd4ed27
Olivier Cheah <oliviercheah@gmail.com>
2020-10-23 00:31:36 +0800

    [Storage - Save] - Save after every commands

---
+++ src/main/java/seedu/duke/storage/TransactionListEncoded.java
@@ -0,0 +1,38 @@
+package seedu.duke.storage;
+
+import seedu.duke.data.Transaction;
+import seedu.duke.data.TransactionList;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionListEncoded {
+    /**
+     * Encodes all the {@code Person} in the {@code toSave} into a list of decodable and readable string presentation
+     * for storage.
+     */
+    public static List<String> encodeTransactionList(TransactionList toSave) {
+        final List<String> encodedTransactions = new ArrayList<>();
+        toSave.getTransactionList().forEach(transaction ->
+                encodedTransactions.add(encodedTransactionToString(transaction)));
+        return encodedTransactions;
+    }
+
+    /**
+     * Encodes the {@code person} into a decodable and readable string representation.
+     */
+    private static String encodedTransactionToString(Transaction transaction) {
+
+        final StringBuilder encodedTransactionBuilder = new StringBuilder();
+        DecimalFormat df2 = new DecimalFormat("#.00");
+        encodedTransactionBuilder.append(transaction.getDate()
+                .equalsIgnoreCase("") ? "-" : transaction.getDate());
+        encodedTransactionBuilder.append(" ");
+        encodedTransactionBuilder.append(transaction.getDescription());
+        encodedTransactionBuilder.append(" ");
+        encodedTransactionBuilder.append("$" + df2.format(transaction.getAmount()));
+
+        return encodedTransactionBuilder.toString();
+    }
+}
