771f31a2ba4c4a5d1c568f5a0d6d9f8f84af67b8
Lai Ping <>
2020-10-25 09:49:47 +0800

    Rebuild with New Intellij Version

--- src/main/java/seedu/duke/commands/ViewBudgetCommand.java
+++ src/main/java/seedu/duke/commands/ViewBudgetCommand.java
@@ -8,11 +8,9 @@ public class ViewBudgetCommand extends Command {
 
     public static final String COMMAND_WORD = "budgetview";
     public static final String MESSAGE_SUCCESS = "Above are all budgets entered.";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": View a list of budgets added to the NUS Expenses Tracker.\n"
             + "Example: " + COMMAND_WORD;
-
 
     @Override
     public CommandResult execute() {
